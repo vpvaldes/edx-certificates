5248aa857a2c39fed0f44c5e744ed066a35b593a
Victoria <vvaldes@alumnos.utalca.cl>
2016-10-27 18:12:36 -0300

    592

--- gen_cert.py
+++ gen_cert.py
@@ -589,8 +589,8 @@ class CertificateGen(object):
 #            styleOpenSans.fontSize = 24
 #            #styleOpenSans.leading = 10
 #            styleOpenSans.leading = 21
-        styleOpenSans.fontSize = 18   
-        styleOpenSans.leading = 10    
+        styleOpenSans.fontSize = 19   
+        styleOpenSans.leading = 21    
         styleOpenSans.textColor = colors.Color(
             0, 0.624, 0.886)
         styleOpenSans.alignment = TA_CENTER
