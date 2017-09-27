8af20ab2977f00d68be72cfd0953299f66baafff
Victoria <vvaldes@alumnos.utalca.cl>
2017-09-27 13:47:54 -0300

    603 - Nombre Curso personalizado

--- gen_cert.py
+++ gen_cert.py
@@ -598,10 +598,10 @@ class CertificateGen(object):
         #paragraph_string = u"<b style='line-height: 2'><i>{0}: {1}</i></b>".format(
         #    self.course, self.long_course.decode('utf-8'))
         #!important course name
-        paragraph_string = u"<b style='line-height: 2'><i>{1}</i></b>".format(
-            self.course, self.long_course.decode('utf-8'))
-        #paragraph_string = u"<b style='line-height: 2'><i>Arte y Espacio Público</i></b>".format(
+        #paragraph_string = u"<b style='line-height: 2'><i>{1}</i></b>".format(
         #    self.course, self.long_course.decode('utf-8'))
+        paragraph_string = u"<b style='line-height: 2'><i>De la desnutrición a la obesidad: un problema de alimentación</i></b>".format(
+            self.course, self.long_course.decode('utf-8'))
         paragraph = Paragraph(paragraph_string, styleOpenSans)
         # paragraph.wrapOn(c, WIDTH * mm, HEIGHT * mm)
         if 'PH207x' in self.course:
